12074013b83425075ef5bf7e19ae452a805db5fe
alimasudd <alimasudd.dev@gmail.com>
2023-10-27 11:01:27 +0700

    create dropdownSearch provinsi asal with data from API

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -17,6 +17,45 @@ class HomeView extends GetView<HomeController> {
         title: const Text('Cek Ongkir'),
         centerTitle: true,
       ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          //provinsi asal
+          DropdownSearch<Province>(
+            popupProps: PopupProps.dialog(
+              fit: FlexFit.loose,
+              showSearchBox: true,
+              itemBuilder: (context, item, isSelected) => ListTile(
+                title: Text('${item.province}'),
+              ),
+            ),
+            dropdownDecoratorProps: const DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: 'Provinsi Asal',
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            asyncItems: (text) async {
+              var response = await Dio().get(
+                "https://api.rajaongkir.com/starter/province",
+                options: Options(
+                  headers: {
+                    'key' : '0ae702200724a396a933fa0ca4171a7e'
+                  }
+                )
+              );
+              var models = Province.fromJsonList(response.data['rajaongkir']['results']);
+              return models;
+            },
+            onChanged: (value) => controller.provAsalId.value = value!.provinceId ?? '0',
+          ),
+          SizedBox(height: 20,),
+        ],
+      )
     );
   }
 }
